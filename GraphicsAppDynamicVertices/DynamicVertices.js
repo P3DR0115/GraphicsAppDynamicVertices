0d8663669c151b76b69554d69f7bd392b3d17882
Pedro A. Tresgallo <P3dro115@gmail.com>
2018-11-25 22:06:45 -0600

    Add different model options Add logic for switching between models Add logic for all models having the same (Functioning) features
    Fixed bug with multiples of the same model being able to be loaded in. (AKA spamming a load model button)

--- GraphicsAppDynamicVertices/DynamicVertices.js
+++ GraphicsAppDynamicVertices/DynamicVertices.js
@@ -1,56 +1,107 @@
-﻿var VSHADER_SOURCE =
-    'attribute vec4 a_Position;\n' +
-    'attribute vec4 a_Color;\n' +
-    'uniform mat4 u_MvpMatrix;\n' +
-    'varying vec4 v_Color;\n' +
-    'void main() {\n' +
-    '  gl_Position = u_MvpMatrix * a_Position;\n' +
-    '  v_Color = a_Color;\n' +
-    '}\n';
+﻿var mainScene;
 
-// Fragment shader program
-var FSHADER_SOURCE =
-    'precision mediump float;\n' +
-    'varying vec4 v_Color;\n' +
-    'void main() {\n' +
-    '  gl_FragColor = v_Color;\n' +
-    '}\n';
+// Camera Variables
+var camera;
+var cFov, cAspect, cNear, cFar, cPosX, cPosY, cPosZ;
+var controls;
+var renderer;
 
+// SpotLight Variables
+var pointLight;
+var sphereSize;
+var pointLightHelper;
 
-var gl;// = getWebGLContext(canvas);
-var n;
-var u_MvpMatrix;
-var vertices;
-var colors;
-var colorchanged = false;
+// Cube Variables
+var colorNumber; // = 0xff0000;
+var material;
 
+var cubeExists;
+var cWidth, cHeight, cDepth;
+var cubeGeometry; 
+var cube;
 
-var mainScene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(90, 1, 0.1, 100);
-var renderer = new THREE.WebGLRenderer();
-var cubeGeometry = new THREE.BoxGeometry(3, 3, 3, 2, 2, 2);
-var colorNumber = 0xff0000;
-var material = new THREE.MeshBasicMaterial({ color: colorNumber });
-var cube = new THREE.Mesh(cubeGeometry, material);
+var wWidth, wHeight, wDepth;
+var wallGeometry;
+var wall;
 
-function main()
+var modelXSpeed;
+var modelYSpeed;
+
+// Torus Vairables
+var torusExists;
+var tRadius, tTube, tRadialSeg, tTubularSeg, tArc;
+var torusGeometry;
+var torus;
+
+var octohedronExists;
+var oRadius, oDetail;
+var octohedronlGeometry;
+var octohedron;
+
+//This is where variables get initialized to their default values
+function InitializeVars()
 {
-    //var canvas = document.getElementById('webgl');
-    //gl = getWebGLContext(canvas);
+    mainScene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(90, 1, 0.1, 100);
+    renderer = new THREE.WebGLRenderer();
 
-    //var object = new THREE.Object3D();
+    pointLight = new THREE.PointLight(0x0000ff, 10, 100);
+    pointLight.position.set(0, 1, 3.1);
+    mainScene.add(pointLight);
+    sphereSize = 1;
+    pointLightHelper = new THREE.PointLightHelper(pointLight, sphereSize);
+    mainScene.add(pointLightHelper);
+    
+    //cubeGeometry.colors[0] = new THREE.Color(0, 255, 255);
+    //cubeGeometry.colors[1] = new THREE.Color(255, 0, 255);
+
+    //colorNumber = 0xffff00;
+    //material = new THREE.MeshBasicMaterial({ color: colorNumber });
+    //wWidth = wHeight = 6; wDepth = 1;
+    //wallGeometry = new THREE.BoxGeometry(wWidth, wHeight, wDepth, 2, 2, 2);
+    //wall = new THREE.Mesh(wallGeometry, material);
+    cubeExists = false;
+    torusExists = false;
+    OctohedralExists = false;
+
     mainScene.autoUpdate = true;
     renderer.setSize(window.innerWidth / 3, window.innerWidth / 3);
     document.body.appendChild(renderer.domElement);
-    mainScene.add(camera);
-    camera.position.z = 5;
-    mainScene.add(cube);
 
-    var animate = function () {
+    mainScene.add(camera);
+    //controls = new THREE.OrbitControls(camera);
+    //mainScene.add(controls);
+    cPosX = cPosY = 0;
+    cPosZ = 10;
+    camera.position.z = cPosZ;
+    //mainScene.add(wall);
+
+    modelXSpeed = 0.00;
+    modelYSpeed = 0.00;
+}
+
+function main()
+{
+    InitializeVars();
+
+    var animate = function ()
+    {
         requestAnimationFrame(animate);
 
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
+        if (cubeExists) {
+            cube.rotation.x += modelXSpeed;
+            cube.rotation.y += modelYSpeed;
+        }
+        else if (torusExists)
+        {
+            torus.rotation.x += modelXSpeed;
+            torus.rotation.y += modelYSpeed;
+        }
+        else if (octohedronExists)
+        {
+            octohedron.rotation.x += modelXSpeed;
+            octohedron.rotation.y += modelYSpeed;
+        }
 
         //MoveColorUp();
 
@@ -59,125 +110,108 @@ function main()
 
     animate();
     
-    /*
-    //var gl = getWebGLContext(canvas);
-    if (!gl) {
-        console.log('Failed to get the rendering context for WebGL');
-        return;
-    }
-
-    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
-        console.log('Failed to intialize shaders.');
-        return;
-    }
-
-     n = initVertexBuffers(gl);
-    if (n < 0) {
-        console.log('Failed to set the vertex information');
-        return;
-    }
-
-    // Set the clear color and enable the depth test
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    gl.enable(gl.DEPTH_TEST);
-
-     u_MvpMatrix = gl.getUniformLocation(gl.program, 'u_MvpMatrix');
-    if (!u_MvpMatrix) {
-        console.log('Failed to get the storage location of u_MvpMatrix');
-        return;
-    }
-
-    // Set the eye point and the viewing volume
-    var mvpMatrix = new Matrix4();
-    mvpMatrix.setPerspective(30, 1, 1, 100);
-    mvpMatrix.lookAt(3, 3, 7, 0, 0, 0, 0, 1, 0);
-
-    gl.uniformMatrix4fv(u_MvpMatrix, false, mvpMatrix.elements);
-
-    Refresh(gl, n);
-    Draw(gl, n);
-    */
 }
 
-function Draw(gl, n)
+function CubeSelect()
 {
-    var cube = new THREE.cub
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    gl.drawElements(gl.TRIANGLES, n, gl.UNSIGNED_BYTE, 0);
-    Refresh(gl, n);
+    if (torusExists)
+    {
+        mainScene.remove(torus);
+        torusExists = false;
+    }
+    if (octohedronExists)
+    {
+        mainScene.remove(octohedron);
+        octohedronExists = false;
+    }
+    if (!cubeExists)
+    {
+        colorNumber = 0xff0000;
+        material = new THREE.MeshBasicMaterial({ color: colorNumber });
+
+        cWidth = cHeight = cDepth = 3;
+        cubeGeometry = new THREE.BoxGeometry(cWidth, cHeight, cDepth, 2, 2, 2);
+        cube = new THREE.Mesh(cubeGeometry, material);
+
+        mainScene.add(cube);
+        cubeExists = true;
+    }
 }
 
-function initVertexBuffers(gl) {
-    vertices = new Float32Array([
-        1.0, 1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, 1.0, 1.0, -1.0, 1.0,  // v0-v1-v2-v3 front
-        1.0, 1.0, 1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, 1.0, 1.0, -1.0,  // v0-v3-v4-v5 right
-        1.0, 1.0, 1.0, 1.0, 1.0, -1.0, -1.0, 1.0, -1.0, -1.0, 1.0, 1.0,  // v0-v5-v6-v1 up
-        -1.0, 1.0, 1.0, -1.0, 1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, 1.0,  // v1-v6-v7-v2 left
-        -1.0, -1.0, -1.0, 1.0, -1.0, -1.0, 1.0, -1.0, 1.0, -1.0, -1.0, 1.0,  // v7-v4-v3-v2 down
-        1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, -1.0   // v4-v7-v6-v5 back
-    ]);
+function TorusSelect()
+{
+    if (cubeExists)
+    {
+        mainScene.remove(cube);
+        cubeExists = false;
+    }
+    if (octohedronExists)
+    {
+        mainScene.remove(octohedron);
+        octohedronExists = false;
+    }
+    if (!torusExists)
+    {
+        colorNumber = 0x005555;
+        material = new THREE.MeshBasicMaterial({ color: colorNumber });
 
-    colors = new Float32Array([
-        0.4, 0.4, 1.0, 0.4, 0.4, 1.0, 0.4, 0.4, 1.0, 0.4, 0.4, 1.0,  // v0-v1-v2-v3 front(blue)
-        0.4, 1.0, 0.4, 0.4, 1.0, 0.4, 0.4, 1.0, 0.4, 0.4, 1.0, 0.4,  // v0-v3-v4-v5 right(green)
-        1.0, 0.4, 0.4, 1.0, 0.4, 0.4, 1.0, 0.4, 0.4, 1.0, 0.4, 0.4,  // v0-v5-v6-v1 up(red)
-        1.0, 1.0, 0.4, 1.0, 1.0, 0.4, 1.0, 1.0, 0.4, 1.0, 1.0, 0.4,  // v1-v6-v7-v2 left
-        1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,  // v7-v4-v3-v2 down
-        0.4, 1.0, 1.0, 0.4, 1.0, 1.0, 0.4, 1.0, 1.0, 0.4, 1.0, 1.0   // v4-v7-v6-v5 back
-    ]);
+        tRadius = 5;
+        tTube = 1;
+        tRadialSeg = 24;
+        tTubularSeg = 24;
+        tArc = 8;
+        torusGeometry = new THREE.TorusGeometry(tRadius, tTube, tRadialSeg, tTubularSeg);//, tArc);
+        torus = new THREE.Mesh(torusGeometry, material);
 
-    var indices = new Uint8Array([
-        0, 1, 2, 0, 2, 3,    // front
-        4, 5, 6, 4, 6, 7,    // right
-        8, 9, 10, 8, 10, 11,    // up
-        12, 13, 14, 12, 14, 15,    // left
-        16, 17, 18, 16, 18, 19,    // down
-        20, 21, 22, 20, 22, 23     // back
-    ]);
-
-    var indexBuffer = gl.createBuffer();
-    if (!indexBuffer)
-        return -1;
-
-    if (!initArrayBuffer(gl, vertices, 3, gl.FLOAT, 'a_Position'))
-        return -1;
-
-    if (!initArrayBuffer(gl, colors, 3, gl.FLOAT, 'a_Color'))
-        return -1;
-
-    // Write the indices to the buffer object
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
-
-    return indices.length;
+        mainScene.add(torus);
+        torusExists = true;
+    }
 }
 
-function initArrayBuffer(gl, data, num, type, attribute) {
-    var buffer = gl.createBuffer();
-    if (!buffer) {
-        console.log('Failed to create the buffer object');
-        return false;
+function OctohedronSelect()
+{
+    if (cubeExists)
+    {
+        mainScene.remove(cube);
+        cubeExists = false;
     }
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-    gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
-
-    var a_attribute = gl.getAttribLocation(gl.program, attribute);
-    if (a_attribute < 0) {
-        console.log('Failed to get the storage location of ' + attribute);
-        return false;
+    if (torusExists)
+    {
+        mainScene.remove(torus);
+        torusExists = false;
     }
-    gl.vertexAttribPointer(a_attribute, num, type, false, 0, 0);
+    if (!octohedronExists)
+    {
+        colorNumber = 0xff00ff;
+        material = new THREE.MeshBasicMaterial({ color: colorNumber });
 
-    // Enable the assignment of the buffer object to the attribute variable
-    gl.enableVertexAttribArray(a_attribute);
+        oRadius = 5;
+        oDetail = 0;
 
-    return true;
+        octohedronlGeometry = new THREE.OctahedronGeometry(oRadius, oDetail);
+        octohedron = new THREE.Mesh(octohedronlGeometry, material);
+
+        mainScene.add(octohedron);
+        octohedronExists = true;
+    }
+}
+
+function adjustCamera()
+{
+    camera.position.x = cPosX;
+    camera.position.y = cPosY;
+    camera.position.z = cPosZ;
+
+    camera.position.set(cPosX, cPosY, cPosZ);
+    //controls.update();
 }
 
 function MoveColorUp()
 {
     //var temp = colorNumber.toString();
+
+    var tempColor;
+    var tempmaterial; // = new THREE.MeshBasicMaterial({ color: colorNumber });
 
     switch (colorNumber)
     {
@@ -197,9 +231,43 @@ function MoveColorUp()
                 break;
             }
     }
+    //colorNumber = new THREE.color(colorNumber);
+    //tempmaterial = new THREE.MeshBasicMaterial({ color: tempColor });
     material = material.color.set(colorNumber); //new THREE.MeshBasicMaterial({ color: colorNumber });
     //cube = new THREE.Mesh(cubeGeometry, material);
     cubeGeometry.elementsNeedUpdate = true;
     cubeGeometry.colorsNeedUpdate = true;
 
+}
+
+function XSpeedUp()
+{
+    modelXSpeed += 0.01;
+}
+
+function XSpeedDown()
+{
+    modelXSpeed -= 0.01;
+}
+
+function YSpeedUp()
+{
+    modelYSpeed += 0.01;
+}
+
+function YSpeedDown()
+{
+    modelYSpeed -= 0.01;
+}
+
+function CameraHeightUp()
+{
+    cHeight += 0.5;
+    adjustCamera();
+}
+
+function CameraHeightDown()
+{
+    cHeight -= 0.5;
+    adjustCamera();
 }
